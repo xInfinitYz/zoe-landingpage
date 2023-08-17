93726ed5ea141052fc8318c156178ee2a44d6bb4
Oleksandr Kyselov <oleksandr_kyselov@hotmail.com>
2023-08-17 20:23:49 +0200

    - arrangd typed text being fixed/sticky

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -48,9 +48,9 @@ const Slideshow = () => {
 						<div className="absolute top-0 sm:left-0 left-3/3 w-8/12 sm:w-4/12 md:w-3/12 lg:w-2/12 h-24 z-10"> 
                             <Image src="/images/zc-logo-text-white.png" loading="lazy" objectFit="contain" layout="responsive" width={600} height={300} />
                         </div>
-						<div className="absolute z-10 flex flex-col justify-around items-center inset-0">
-				
-							<h1 className="text-center md:text-6xl text-4xl font-bold text-white mb-36 fixed">
+						<div className="absolute z-10 flex flex-col items-center ">
+							<div className="flex-1">
+							<h1 className="h-2 text-center md:text-6xl text-4xl font-bold text-white mb-36">
 								<Typed
 									strings={["I'm Zoe.", "Shop now!"]}
 									typeSpeed={100}
@@ -61,6 +61,7 @@ const Slideshow = () => {
 									loop
 								/>
 							</h1>
+							</div>
 							{/*<p className="text-center md:text-2xl text-xl font-bold text-white">
 							<Typed
 									strings={["Shop now!"]}
@@ -70,16 +71,18 @@ const Slideshow = () => {
 									onComplete={() => setButtonVisible(true)}
 								/>
 				</p>*/}
+				<div className="flex-1">
 							<button className="transition-opacity mt-4 py-2 px-6 border border-white text-white hover:bg-white hover:text-black focus:outline-none duration-1500 ease-in-out"
 								onClick={() => { window.location.href = "https://store.zoecosentino.com" }}>
 								   Go to the store!
 							</button>
+							</div>
 
-
-							<div className="absolute bottom-0 w-full text-center pb-4 text-sm">
+						
+						</div>
+						<div className="absolute bottom-0 w-full text-center pb-4 text-sm">
 								<span className="text-white">Made with ❤️ &nbsp; by Oleksandr Kyselov</span>
 							</div>
-						</div>
 					</div>
 				))}
 			</Zoom>
